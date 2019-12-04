fa3ba34451fa8bfb38bb35798f4765cb94368456
Micha <mlk04@wp.pl>
2019-12-04 18:54:21 +0100

    temperature node

--- lib/tmp/tmp_node.py
+++ lib/tmp/tmp_node.py
@@ -1,12 +1,34 @@
 import time
 from tmp102 import TMP102
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
 
-# Initialize TMP Object
-tmp = TMP102('C', 0x48, 1)
+class TmpPublisher(Node):
 
-# Actually, lets make the temperatures Farenheit
-tmp.setUnits('F')
+    def __init__(self):
+        super().__init__('tmp_publisher')
+        self.publisher_ = self.create_publisher(String, 'tmp', 10)
+        self.tmp = TMP102('C', 0x48, 1)
+        self.tmp.setUnits('C')
+        timer_period = 0.5 #seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
 
-while True:
-    print "Current temp: {:.1f}degF".format(tmp.readTemperature())
-    time.sleep(1)
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Current temp: {:.1f}degF".format(tmp.readTemperature())
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+def main(args=None):
+    rclpy.init(args=args)
+    tmp_publisher = TmpPublisher()
+    rclpy.spin(tmp_publisher)
+    tmp_publisher.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
