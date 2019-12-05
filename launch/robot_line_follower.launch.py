b2ebcb3b0c70544906c99f7f278a81aec007875e
Your Name <you@example.com>
2019-12-05 16:19:11 +0000

    added launch file

---
+++ launch/robot_line_follower.launch.py
@@ -0,0 +1,11 @@
+from launch import LaunchDescription
+import launch_ros.actions
+ 
+ 
+def generate_launch_description():
+    return LaunchDescription([
+        launch_ros.actions.Node(
+            # the name of the executable is set in CMakeLists.txt, towards the end of
+            # the file, in add_executable(...) and the directives following it
+            package='robot_line_follower', node_executable='tmp_node.py', output='screen'),
+    ])
