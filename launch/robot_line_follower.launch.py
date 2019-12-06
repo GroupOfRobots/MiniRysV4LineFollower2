81e57e9dd2244ab23e5247b5c942238536664e7c
Your Name <you@example.com>
2019-12-06 16:00:17 +0000

    launch ready

--- launch/robot_line_follower.launch.py
+++ launch/robot_line_follower.launch.py
@@ -1,15 +1,27 @@
 from launch import LaunchDescription
+from ament_index_python.packages import get_package_prefix
 import launch_ros.actions
+import os
+package = "robot_line_follower"
  
  
 def generate_launch_description():
-    return LaunchDescription([
+    path = get_package_prefix(package)
+    path = path[1:]
+    path_list = path.split('/')
+    print(path_list)
+    del path_list[-2:]
+    full_path = ''
+    for path in path_list:
+        full_path = full_path + '/' + path
 
-        launch_ros.actions.Node(
+    print(full_path)
+    return LaunchDescription([
+    launch_ros.actions.Node(
             # the name of the executable is set in CMakeLists.txt, towards the end of
             # the file, in add_executable(...) and the directives following it
-            package='robot_line_follower', node_executable='tmp_node.py', output='screen', 
-	    parameters=["tmp_node_params.yaml"]),
+            package= package, node_executable='tmp_node.py', output='screen', 
+            parameters=[full_path + "/src/" + package + "/yaml/tmp_node_params.yaml"]),
 
 	launch_ros.actions.Node(
             # the name of the executable is set in CMakeLists.txt, towards the end of
