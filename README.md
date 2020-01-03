62235905c03eff829e4a4e612e7c8861d001ab2f
Your Name <you@example.com>
2020-01-03 13:16:58 +0000

    decoder app for visualizing robot stream in rviz2

--- README.md
+++ README.md
@@ -1,11 +1,18 @@
 # robot_line_follower
 Package of line follower functionality under ROS2 environment.
 
-## Running package: 
-1. cd <your_ws> 
-2. colcon build 
-3. source install/setup.bash 
-4. ros2 launch robot_line_follower robot_line_follower.launch.py 
+## Running package on robot: 
+1. `cd <your_ws>` 
+2. `colcon build` 
+3. `source install/setup.bash` 
+4. `ros2 launch robot_line_follower robot_line_follower.launch.py` - for test
+5. `ros2 launch robot_line_follower data_pub_example.launch.py` - to run developed version 
+
+## Running package on PC: 
+1. `cd <your_ws>` 
+2. `colcon build` 
+3. `source install/setup.bash` 
+4. `ros2 run robot_line_follower client` - for decoding image sent from robot in order to visualize it in rviz2
 
 ## Parameters
 All parameters can be changed in files in `yaml` directory.
--- src/client.cpp
+++ src/client.cpp
@@ -3,37 +3,63 @@
 #include "sensor_msgs/msg/image.hpp"
 #include <sensor_msgs/msg/compressed_image.hpp>
 #include <opencv2/opencv.hpp>
+#include <boost/endian/conversion.hpp>
+#include <sensor_msgs/image_encodings.hpp>
 using namespace std::chrono_literals;
 using std::placeholders::_1;
+namespace enc = sensor_msgs::image_encodings;
 
 class Decoder : public rclcpp::Node
 {
-  public:
-    Decoder(): Node("decoder")
-	{
-    	publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image");
-		subscription_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
-    	"compressed_image", 10, std::bind(&Decoder::topic_callback, this, _1));
-    }
+	public:
+		Decoder(): Node("decoder")
+		{
+			publisher_ = this->create_publisher<sensor_msgs::msg::Image>("image");
+			subscription_ = this->create_subscription<sensor_msgs::msg::CompressedImage>(
+			"compressed_image", 10, std::bind(&Decoder::topic_callback, this, _1));
+		}
 
 	private:
-		void topic_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg) const
-    	{
-    		cv::Mat image = cv::imdecode(cv::Mat(msg->data),1);
-		sensor_msgs::msg::Image::SharedPtr ptr = std::make_shared<sensor_msgs::msg::Image>();
+		void topic_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg)
+		{
+			cv::Mat image = cv::imdecode(cv::Mat(msg->data),1);
+			sensor_msgs::msg::Image ros_image;
+			rclcpp::Time timestamp = this->get_clock()->now();
+			ros_image.header.stamp = timestamp;
+			ros_image.header.frame_id = "image";
 		
-    		cv::imshow("view", image);
-			cv::waitKey(10);
-    	}
+			ros_image.height = image.rows;
+			ros_image.width = image.cols;
+			ros_image.encoding = enc::BGR8;
+			ros_image.is_bigendian = (boost::endian::order::native == boost::endian::order::big);
+			ros_image.step = image.cols * image.elemSize();
+			size_t size = ros_image.step * image.rows;
+			ros_image.data.resize(size);
 
-    	rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
+			if (image.isContinuous()) {
+				memcpy(reinterpret_cast<char *>(&ros_image.data[0]), image.data, size);
+			} 
+			
+			else {
+				uchar * ros_data_ptr = reinterpret_cast<uchar *>(&ros_image.data[0]);
+				uchar * cv_data_ptr = image.data;
+				for (int i = 0; i < image.rows; ++i) {
+					memcpy(ros_data_ptr, cv_data_ptr, ros_image.step);
+					ros_data_ptr += ros_image.step;
+					cv_data_ptr += image.step;
+				}
+			}
+			publisher_->publish(ros_image);
+		}
+
+		rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr publisher_;
 		rclcpp::Subscription<sensor_msgs::msg::CompressedImage>::SharedPtr subscription_;
-  };
+};
 
-  int main(int argc, char * argv[])
-  {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<Decoder>());
-    rclcpp::shutdown();
-    return 0;
-  }
+int main(int argc, char * argv[])
+{
+	rclcpp::init(argc, argv);
+	rclcpp::spin(std::make_shared<Decoder>());
+	rclcpp::shutdown();
+	return 0;
+}
